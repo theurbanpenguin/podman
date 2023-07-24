ca93c3eb1b86ec392bf02642057510d0f83e7c5b
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-07-24 11:13:13 +0100

    Develop 2nd section

--- build.sh
+++ build.sh
@@ -18,17 +18,19 @@ podman continer ls -a
 ls -la ~
 tree ~/.local
 cat /etc/containers/registries.conf
-grep "hello" /etc/containers/registries.d/000-shortnames.conf
-
-podman container ls -a --format "{{.Names}}"
-podman container start $(podman container ls -a --format "{{.Names}}")
-podman container logs $(podman container ls -a --format "{{.Names}}")
-podman container prune -f
-podman image ls
+grep "hello" /etc/containers/registries.conf.d/000-shortnames.conf
+podmam image search fedora
+podman image search ubuntu
 podman image pull docker.io/library/fedora
+podman image pull docker.io/library/ubuntu
 podman image ls
 podman image inspect fedora
 podman image inspect fedora --format "{{json .Config}}"
 podman image inspect fedora --format "{{json .Config.Cmd}}"
 read -p "This is 2nd section, do you want to continue y/n: "
 if [ "$REPLY" == "n" ]; then exit 1; fi
+
+podman container ls -a --format "{{.Names}}"
+podman container start $(podman container ls -a --format "{{.Names}}")
+podman container logs $(podman container ls -a --format "{{.Names}}")
+podman container prune -f
