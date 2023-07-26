e7533436199ad9af8031e9846f46092b15aaad61
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-07-26 11:36:39 +0100

    add web

--- build.sh
+++ build.sh
@@ -23,4 +23,6 @@ if [ "$REPLY" == "n" ]; then exit 1; fi
 
 sudo yum install -y w3m
 podman image pull docker.io/ubuntu/apache2
-
+sudo chcon -Rt container_file_t /home/tux/podman/www
+podman container run -d --name www -e TZ='Europe/London' -p 8080:80 -v /home/tux/podman/www:/var/www/html apache2:latest
+podman container rm -f www
